e1a52108237cff4d3e8b2a08d7c5853cecac8ae7
ZayJayPlays <116956084+ZayJayPlays@users.noreply.github.com>
2023-02-22 08:09:11 -0700

    FirstCommit

---
+++ LoginScreen/Backup.swift
@@ -0,0 +1,8 @@
+//
+//  Backup.swift
+//  LoginScreen
+//
+//  Created by Zane Jones on 2/21/23.
+//
+
+import Foundation
--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var usernameTextBox: UITextField!
+    @IBOutlet var passwordTextBox: UITextField!
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var submitButton: UIButton!
+    
+    let specialCharSet = CharacterSet(charactersIn: "`~!@#$%^&*()-=_+{}|[]\'<>?,.'")
+    let numberCharSet = CharacterSet(charactersIn: "0123456789")
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.isHidden = true
     }
-
-
+    
+    
+    @IBAction func submitCredentials() {
+        guard usernameTextBox.text != nil else {
+            showFailure()
+            return
+        }
+        guard passwordTextBox.text != nil  else { showFailure()
+            return
+        }
+        guard (passwordTextBox.text?.rangeOfCharacter(from: numberCharSet)) != nil && (passwordTextBox.text?.rangeOfCharacter(from: specialCharSet)) != nil else {
+            showFailure()
+            return
+        }
+        
+        guard passwordTextBox.text!.count >= 8  else {
+            showFailure()
+            return
+        }
+        messageLabel.text = "Success"
+        messageLabel.textColor = UIColor.green
+        messageLabel.isHidden = false;
+    }
+                
+    func showFailure() {
+        messageLabel.text = "Failure"
+        messageLabel.textColor = UIColor.red
+        messageLabel.isHidden = false;
+    }
 }
 
